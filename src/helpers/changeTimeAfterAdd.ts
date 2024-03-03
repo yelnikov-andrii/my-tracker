307023fa84acab49dae932e959830df0deefb980
Andrii Yelnikov <whiteyelnikov@gmail.com>
2024-03-03 15:14:54 +0200

    fixed changing time after adding a deal

--- src/helpers/changeTimeAfterAdd.ts
+++ src/helpers/changeTimeAfterAdd.ts
@@ -1,21 +1,14 @@
 import { setFinishHour, setFinishMinutes, setStartHour, setStartMinutes } from "../store/timeSlice";
 
 export const changeTimeAfterAddingAdeal = (startHour: string, finishHour: string, finishMinutes: string, dispatch: any) => {
-  if (startHour === finishHour) {
-    if (finishMinutes === '55') {
-      const newStartHour = (+startHour + 1).toString().padStart(2, '0');
-      const newFinishHour = (+finishHour + 1).toString().padStart(2, '0');
-      dispatch(setStartHour(newStartHour));
-      dispatch(setStartMinutes('00'));
-      dispatch(setFinishHour(newFinishHour));
-      dispatch(setFinishMinutes('05'));
-    } else {
-      dispatch(setStartMinutes(finishMinutes));
-      dispatch(setFinishMinutes((+finishMinutes + 5).toString().padStart(2, '0')));
-    }
-  } else {
     dispatch(setStartHour(finishHour));
+    dispatch(setFinishHour(finishHour));
     dispatch(setStartMinutes(finishMinutes));
     dispatch(setFinishMinutes((+finishMinutes + 5).toString().padStart(2, '0')));
-  }
+
+    if (finishMinutes === '55') {
+      const newStartHour = (+finishHour + 1).toString().padStart(2, '0');
+      dispatch(setFinishHour(newStartHour));
+      dispatch(setFinishMinutes('00'));
+    }
 }
