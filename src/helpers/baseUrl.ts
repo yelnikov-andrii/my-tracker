0299b871d7f59d493d086fd3c6d152652c47727b
Andrii <whiteyelnikov@gmail.com>
2024-11-04 20:02:58 +0200

    added deleting all completed todos and changed base url

--- src/helpers/baseUrl.ts
+++ src/helpers/baseUrl.ts
@@ -1,2 +1,2 @@
-// export const baseUrl = 'https://tracker-back-uf9y.onrender.com';
-export const baseUrl = 'http://localhost:2000';
+export const baseUrl = 'https://tracker-back-uf9y.onrender.com';
+// export const baseUrl = 'http://localhost:2000';
