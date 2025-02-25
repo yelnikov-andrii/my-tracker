4b6410b443389ce48400c4b484034fc8dad3f4f7
Andrii <whiteyelnikov@gmail.com>
2025-02-25 11:58:22 +0200

    made calendar changeble

--- src/Components/UI/MyCalendar.tsx
+++ src/Components/UI/MyCalendar.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { setDate } from '../../store/timeSlice';
 
-export const MyCalendar: React.FC<any> = ({ value, onChange }) => {
+export const MyCalendar: React.FC<any> = () => {
+  const currentDate = useSelector((state: RootState) => state.time.currentDate);
+  const dispatch = useDispatch();
+
+  function changeDate(date: any) {
+    dispatch(setDate(date));
+  }
   return (
     <div>
-      <Calendar 
-        onChange={onChange} 
-        value={value}
+      <Calendar
+        onChange={changeDate}
+        value={currentDate}
         locale='ua-UA'
       />
     </div>
